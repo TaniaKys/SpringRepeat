5516a5a03ca5ed4e1420a29e53e3304472a7a7ae
tania <tetiana.kysla@igniteoutsourcing.com>
2017-08-30 15:10:27 +0300

    test controller

--- src/main/java/com/starr/springrepeat/controller/MyController.java
+++ src/main/java/com/starr/springrepeat/controller/MyController.java
@@ -15,36 +15,50 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class MyController {
 
-    @RequestMapping(value="/jsonPublic", method = RequestMethod.GET)
+    @RequestMapping(value = "/jsonPublic", method = RequestMethod.GET)
     @JsonView(User.Public.class)
     public User getPublic() {
         return new User();
     }
 
-    @RequestMapping(value="/jsonRoot", method = RequestMethod.GET)
+    @RequestMapping(value = "/jsonRoot", method = RequestMethod.GET)
     @JsonView(User.Root.class)
     public User getRoot() {
         return new User();
     }
 
 
-    @RequestMapping(value="/getUser", method = RequestMethod.GET)
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     @JsonView(User.Root.class)
     public User getUser(HttpServletRequest request) {
         ServletContext context = request.getServletContext();
-        if (context.getAttribute("user") == null){
+        if (context.getAttribute("user") == null) {
             User newUser = new User("tania", "123456");
             context.setAttribute("user", newUser);
         }
         return (User) context.getAttribute("user");
     }
 
-    @RequestMapping(value="/editUser", method = RequestMethod.POST)
+    @RequestMapping(value = "/editUser", method = RequestMethod.POST)
     public ResponseEntity<User> editUser(HttpServletRequest request, @RequestBody User user) {
         ServletContext context = request.getServletContext();
-        if (context.getAttribute("user") != null){
-            context.setAttribute("user", user);
+        context.setAttribute("user", user);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public ResponseEntity<User> addUser(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        ServletContext context = request.getServletContext();
+        User user = (User)context.getAttribute("user");
+        if (user == null) {
+            user = new User(username, password);
+        } else {
+            user.setUsername(username);
+            user.setPassword(password);
         }
+        context.setAttribute("user", user);
         return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 }
--- src/main/java/com/starr/springrepeat/entity/User.java
+++ src/main/java/com/starr/springrepeat/entity/User.java
@@ -26,17 +26,25 @@ public class User {
         this.password = password;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Getter
-    @Setter
     private Long id;
 
-    @Getter
-    @Setter
     private String username;
 
-    @Getter
-    @Setter
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
