ef6a20b9aa5be9143ac7e14d75253d893b9d2484
tania <tetiana.kysla@igniteoutsourcing.com>
2017-08-30 14:51:29 +0300

    test controller

--- src/main/java/com/starr/springrepeat/controller/MyController.java
+++ src/main/java/com/starr/springrepeat/controller/MyController.java
@@ -2,9 +2,15 @@ package com.starr.springrepeat.controller;
 
 import com.starr.springrepeat.entity.User;
 import org.codehaus.jackson.map.annotate.JsonView;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MyController {
@@ -22,4 +28,23 @@ public class MyController {
     }
 
 
+    @RequestMapping(value="/getUser", method = RequestMethod.GET)
+    @JsonView(User.Root.class)
+    public User getUser(HttpServletRequest request) {
+        ServletContext context = request.getServletContext();
+        if (context.getAttribute("user") == null){
+            User newUser = new User("tania", "123456");
+            context.setAttribute("user", newUser);
+        }
+        return (User) context.getAttribute("user");
+    }
+
+    @RequestMapping(value="/editUser", method = RequestMethod.POST)
+    public ResponseEntity<User> editUser(HttpServletRequest request, @RequestBody User user) {
+        ServletContext context = request.getServletContext();
+        if (context.getAttribute("user") != null){
+            context.setAttribute("user", user);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
 }
